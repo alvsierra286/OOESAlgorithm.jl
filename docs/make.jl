bb47976c7ef05c41dbe086eb13d057b4c8a358cb
Alvaro Sierra-Altamiranda <amsierra@mail.usf.edu>
2019-02-20 13:24:36 -0500

    Add files via upload

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using Documenter, OOESAlg
+
+makedocs(modules=[OOESAlg],
+         doctest = false,
+         format = :html,
+         sitename = "OOESAlg",
+         authors = "Alvaro Sierra-Altamiranda",
+         pages = Any[
+        	"Home" => "index.md",
+        	"Installation" => "installation.md",
+        	"Getting Started" => "getting_started.md",
+        	"Advanced Features" => "advanced.md",
+        	"Solving Instances from Literature" => "solving_instances_from_literature.md"
+    	])
+
+deploydocs(
+	repo = "github.com/alvsierra286/OOESAlg.git",
+    target = "build",
+    osname = "linux",
+    julia  = "1.0",
+    deps   = nothing,
+    make   = nothing,
+)
