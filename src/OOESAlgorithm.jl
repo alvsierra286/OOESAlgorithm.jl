c7f996e35b71fd3eeb72c4e0f4bddcea27656041
Alvaro Sierra-Altamiranda <amsierra@mail.usf.edu>
2019-03-04 10:08:41 -0500

    Update OOESAlgorithm.jl

--- src/OOESAlgorithm.jl
+++ src/OOESAlgorithm.jl
@@ -3,6 +3,29 @@ module OOESAlgorithm
 
 using Distributed, MathOptInterface, JuMP, MathProgBase, GLPK, GLPKMathProgInterface, Pkg, SparseArrays
 
+println("******************************************************************")
+println("******************************************************************")
+println("*****                                                        *****")
+println("*****               Welcome to OOESAlgorithm                 *****")
+println("*****           A comprehensive Julia package for            *****")
+println("***** Bi-Objective Mixed Integer Linear Programming Problems *****")
+println("*****                                                        *****")
+println("***** To support us please cite:                             *****")
+println("*****                                                        *****")
+println("***** Sierra-Altamiranda, A., Charkhgard, H., 2018.          *****")
+println("***** A new exact algorithm to optimize a linear function    *****")
+println("***** over the set of efficient solutions for bi-objective   *****")
+println("***** mixed integer linear programming. INFORMS Journal on   *****")
+println("***** Computing To appear.                                   *****")
+println("*****                                                        *****")
+println("***** Sierra-Altamiranda, A., Charkhgard, H., 2018.          *****")
+println("***** OOESAlgorithm.jl: A julia package for optimizing a     *****")
+println("***** linear function over the set of efficient solutions    *****")
+println("***** for bi-objective mixed integer linear programming      *****")
+println("*****                                                        *****")
+println("******************************************************************")
+println("******************************************************************")
+
 println("Using GLPKSolverMIP as default solver")
 if ("Gurobi" in keys(Pkg.installed()))
 	using Gurobi
